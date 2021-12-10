f838186c41a4a05222141ef59ee7d7679ad901b8
Trabajo <Trabajo@DESKTOP-EJIBAC5>
2021-12-10 08:58:58 +0100

    hola

---
+++ src/org/cuatroviientos/ed/main.java
@@ -0,0 +1,11 @@
+package org.cuatroviientos.ed;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hoooola claaaaaaaase!!!!!");
+		
+	}
+
+}
